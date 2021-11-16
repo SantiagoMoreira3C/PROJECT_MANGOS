f22b660742100dd2c839a37b7b808ef6c15be2af
SantiagoMoreira3C <sanmopa2000@gmail.com>
2021-11-15 23:52:04 -0500

    agrrando vista categoria y producto

---
+++ app/src/main/java/facci/am1/mangos/ProductoActivity.java
@@ -0,0 +1,14 @@
+package facci.am1.mangos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto);
+    }
+}
